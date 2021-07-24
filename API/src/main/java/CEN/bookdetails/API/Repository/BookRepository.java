e83fed61866bfe0c598ac00ddbd4ec41dc0cc8f8
apmendenhall <68875076+apmendenhall@users.noreply.github.com>
2021-07-23 19:19:14 -0700

    Delete API directory

--- API/src/main/java/CEN/bookdetails/API/Repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package CEN.bookdetails.API.Repository;
-
-import CEN.bookdetails.API.Model.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Books, Integer>{
-}
