e5f577eb8d9dc5b18ebbd9e5a63e04ba4ffd20cf
Ziad <zmali002@fiu.edu>
2021-07-30 15:00:53 -0400

    Book Review Feature Added

---
+++ BookReviews/BookReviewConfig.java
@@ -0,0 +1,24 @@
+package com.example.RestAPI.BookReview;
+
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BookReviewConfig {
+    public BookReviewConfig() {
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ReviewRepository repository) {
+        return (args) -> {
+            Review review1 = new Review("Harry Potter and the Goblet of Fire", "Amazing!", 2);
+            Review review2 = new Review("Narnia", "Holy Cow!", 3);
+            repository.saveAll(List.of(review1, review2));
+        };
+    }
+}
+
+
+
---
+++ BookReviews/Review.java
@@ -0,0 +1,128 @@
+
+package com.example.RestAPI.BookReview;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Review {
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "book_sequence"
+    )
+    private Long id;
+    @Column(
+            name = "Title"
+    )
+    private String bookTitle;
+    private String bookReview;
+    private String timeStamp;
+    private Double averageRating;
+    private Integer numberOfRatings = 0;
+    private Integer bookRating;
+
+    public Review() {
+        this.timeStamp = (new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(Calendar.getInstance().getTime());
+    }
+
+    public Review(String bookTitle, String bookReview, Integer bookRating) {
+        this.bookTitle = bookTitle;
+        this.bookReview = bookReview;
+        this.timeStamp = (new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(Calendar.getInstance().getTime());
+        this.averageRating = (double)bookRating;
+        this.numberOfRatings = this.numberOfRatings + 1;
+        this.bookRating = bookRating;
+    }
+
+    public Review(String bookTitle, String bookReview) {
+        this.bookTitle = bookTitle;
+        this.bookReview = bookReview;
+        this.timeStamp = (new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(Calendar.getInstance().getTime());
+    }
+
+    public Review(String bookTitle, Integer bookRating) {
+        this.bookTitle = bookTitle;
+        this.timeStamp = (new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(Calendar.getInstance().getTime());
+        this.averageRating = (double)bookRating;
+        this.numberOfRatings = this.numberOfRatings + 1;
+        this.bookRating = bookRating;
+    }
+
+    public Integer getBookRating() {
+        return this.bookRating;
+    }
+
+    public void setBookRating(Integer bookRating) {
+        this.bookRating = bookRating;
+    }
+
+    public String getBookTitle() {
+        return this.bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookReview() {
+        return this.bookReview;
+    }
+
+    public void setBookReview(String bookReview) {
+        this.bookReview = bookReview;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTimeStamp() {
+        return this.timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void addRating(Integer Rating) {
+        this.averageRating = this.averageRating * (double) this.numberOfRatings / ++this.numberOfRatings;
+    }
+
+    public Double getAverageRating() {
+        return this.averageRating == null ? 0.0D : this.averageRating;
+    }
+
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public Integer getNumberOfRatings() {
+        return this.numberOfRatings;
+    }
+
+    public void setNumberOfRatings(Integer numberOfRatings) {
+        this.numberOfRatings = numberOfRatings;
+    }
+
+    public String toString() {
+        return "Review{id=" + this.id + ", bookTitle='" + this.bookTitle + "', bookReview='" + this.bookReview + "', timeStamp='" + this.timeStamp + "', averageRating=" + this.averageRating + ", numberOfRatings=" + this.numberOfRatings + ", bookRating=" + this.bookRating + "}";
+    }
+}
