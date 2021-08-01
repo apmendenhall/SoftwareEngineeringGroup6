46bbd9f6008b45988519f17b862abadf94e833a3
Ziad <zmali002@fiu.edu>
2021-08-01 02:57:37 -0400

    Delete Request Mapped

--- BookReviews/BookReviewService.java
+++ BookReviews/BookReviewService.java
@@ -2,6 +2,8 @@
 package com.example.RestAPI.BookReview;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,51 +26,63 @@ public class BookReviewService {
 
     public void addNewReview(Review review) {
 
-        if(review.getBookRating() != null){
-            if (review.getBookRating() > 0 && review.getBookRating() < 6) {
-                if (review.getBookTitle().equals((this.repository.findByTitle(review.getBookTitle()).get(0)).getBookTitle())) {
-                    for(int i = this.repository.findByTitle(review.getBookTitle()).size()-1; i > -1; i--) {
-                        Review pastBook = (Review)this.repository.findByTitle(review.getBookTitle()).get(i);
-                        pastBook.addRating(review.getBookRating());
-                        this.repository.save(pastBook);
+        if(review.getBookReview() == null && review.getBookRating() == null){
+            throw new IllegalStateException("Must at least Submit either a Comment or a Rating");
+        }
+        else{
+            if(review.getBookRating() != null){
+                if (review.getBookRating() > 0 && review.getBookRating() < 6) {
+                    if (review.getBookTitle().equals((this.repository.findByTitle(review.getBookTitle()).get(0)).getBookTitle())) {
+                        for(int i = this.repository.findByTitle(review.getBookTitle()).size()-1; i > -1; i--) {
+                            Review pastBook = this.repository.findByTitle(review.getBookTitle()).get(i);
+                            pastBook.addRating(review.getBookRating());
+                            this.repository.save(pastBook);
+                        }
+                        review.setAverageRating(this.repository.findByTitle(review.getBookTitle()).get(0).getAverageRating());
+                        review.setNumberOfRatings(this.repository.findByTitle(review.getBookTitle()).get(0).getNumberOfRatings());
                     }
-                    review.setAverageRating(((Review)this.repository.findByTitle(review.getBookTitle()).get(0)).getAverageRating());
-                    review.setNumberOfRatings(((Review)this.repository.findByTitle(review.getBookTitle()).get(0)).getNumberOfRatings());
+                    this.repository.save(review);
+                }
+                else {
+                    throw new IllegalStateException("Book Rating Cannot be set to:" + review.getBookRating() + ", Ratings are Between 0-5 Inclusive");
+                }
+            }
+            else{
+                if (review.getBookTitle().equals(this.repository.findByTitle(review.getBookTitle()).get(0).getBookTitle())) {
+                    review.setNumberOfRatings(this.repository.findByTitle(review.getBookTitle()).get(0).getNumberOfRatings());
+                    review.setAverageRating(this.repository.findByTitle(review.getBookTitle()).get(0).getAverageRating());
                 }
                 this.repository.save(review);
             }
-            else {
-                throw new IllegalStateException("Book Rating Cannot be set to:" + review.getBookRating() + ", Ratings are Between 0-5 Inclusive");
-            }
-        }
-        else{
-            if (review.getBookTitle().equals(((Review)this.repository.findByTitle(review.getBookTitle()).get(0)).getBookTitle())) {
-                review.setNumberOfRatings(this.repository.findByTitle(review.getBookTitle()).get(0).getNumberOfRatings());
-                review.setAverageRating(this.repository.findByTitle(review.getBookTitle()).get(0).getAverageRating());
-            }
-            this.repository.save(review);
         }
     }
     public void deleteReview(Long studentId) {
         if (!this.repository.existsById(studentId)) {
             throw new IllegalStateException("Review With ID: " + studentId + ", Does Not Exist");
         } else {
-            this.repository.deleteById(studentId);
+            Review review = this.repository.findById(studentId).get();
+                if (review.getBookTitle().equals((this.repository.findByTitle(review.getBookTitle()).get(0)).getBookTitle()) && review.getBookRating() != null) {
+                    review.calculateNewAverageWithDelete(0);
+                    for (int i = this.repository.findByTitle(review.getBookTitle()).size() - 1; i > -1; i--) {
+                        Review pastBook = this.repository.findByTitle(review.getBookTitle()).get(i);
+                        pastBook.setNumberOfRatings(review.getNumberOfRatings());
+                        pastBook.setAverageRating(review.getAverageRating());
+                        this.repository.save(pastBook);
+                    }
+            }this.repository.deleteById(studentId);
         }
     }
 
     @Transactional
     public void updateReview(Long studentId, String bookReview, Integer bookRating) {
-        Review review = (Review)this.repository.findById(studentId).orElseThrow(() -> {
-            return new IllegalStateException("Review With ID: " + studentId + ", Does Not Exist");
-        });
+        Review review = this.repository.findById(studentId).orElseThrow(() -> new IllegalStateException("Review With ID: " + studentId + ", Does Not Exist"));
 
-        if (bookReview != null) {
+        if (bookReview != null && review.getBookReview() != null) {
             if (review.getBookReview().equals(bookReview)) {
                 throw new IllegalStateException("Book Review Cannot be set to:" + bookReview + ", Review is already set to this.");
             }
-            review.setBookReview(bookReview);
         }
+        review.setBookReview(bookReview);
         if (bookRating != null) {
             if (bookRating >= 6 || bookRating <= -1) {
                 throw new IllegalStateException("Book Rating Cannot be set to:" + bookRating + ", Ratings must be between 0-5 Inclusive.");
@@ -78,7 +92,7 @@ public class BookReviewService {
                     review.calculateNewAverage(bookRating);
                     review.setBookRating(bookRating);
                     for (int i = this.repository.findByTitle(review.getBookTitle()).size() - 1; i > -1; i--) {
-                        Review pastBook = (Review) this.repository.findByTitle(review.getBookTitle()).get(i);
+                        Review pastBook = this.repository.findByTitle(review.getBookTitle()).get(i);
                         pastBook.setAverageRating(review.getAverageRating());
                         this.repository.save(pastBook);
                     }
@@ -88,7 +102,7 @@ public class BookReviewService {
                     review.calculateNewAverageWithNull(bookRating);
                     review.setBookRating(bookRating);
                     for (int i = this.repository.findByTitle(review.getBookTitle()).size() - 1; i > -1; i--) {
-                        Review pastBook = (Review) this.repository.findByTitle(review.getBookTitle()).get(i);
+                        Review pastBook = this.repository.findByTitle(review.getBookTitle()).get(i);
                         pastBook.setAverageRating(review.getAverageRating());
                         pastBook.setNumberOfRatings(review.getNumberOfRatings());
                         this.repository.save(pastBook);
